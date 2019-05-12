9f70645bd7a759c41c5a3939b65e8727c45dfda3
Md Shafkat Hussain Tanvir <tanvir0604@gmail.com>
2019-05-12 13:53:06 +0600

    self customized api call

--- src/Rest/Api.php
+++ src/Rest/Api.php
@@ -1,7 +1,15 @@
 <?php
 namespace Tanvir\Sabre\Rest;
 use Tanvir\Sabre\Rest\Api\BargainFinderMax;
+use Tanvir\Sabre\Rest\Call;
 class Api{
+
+    public function call($path, $request)
+    {
+        $Call = new Call();
+        $result = $Call->executePostCall($path, $request);
+        return $result;
+    }
     public function bergainFinderMax($origin, $destination, $departureDate)
     {
         $BargainFinderMax = new BargainFinderMax($origin, $destination, $departureDate);
--- src/Soap/Api.php
+++ src/Soap/Api.php
@@ -1,7 +1,16 @@
 <?php
 namespace Tanvir\Sabre\Soap;
+use Tanvir\Sabre\Soap\Call;
 use Tanvir\Sabre\Soap\Api\BargainFinderMax;
 class Api{
+
+    public function call($action, $request)
+    {
+        $soapClient = new Call($action);
+        $soapClient->setLastInFlow(false);
+        $result = $soapClient->doCall($request);
+        return $result;
+    }
     public function bergainFinderMax($origin, $destination, $departureDate)
     {
         $BargainFinderMax = new BargainFinderMax($origin, $destination, $departureDate);
