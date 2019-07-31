67df8dc5711c665326f007c841b4a7aba50837d3
Md Shafkat Hussain Tanvir <tanvir0604@gmail.com>
2019-07-31 12:22:43 +0600

    PNR api

--- src/Rest/Api/CreatePassengerNameRecord.php
+++ src/Rest/Api/CreatePassengerNameRecord.php
@@ -4,7 +4,7 @@ use Tanvir\Sabre\Rest\Call;
 class CreatePassengerNameRecord{
     public function __construct($params)
     {
-        $this->path = '/v2.2.0/passenger/records';
+        $this->path = '/v2.2.0/passenger/records?mode=create';
         $this->params = $params;
         if(!$this->validateParams()){
             throw new \Exception("Error Processing Request. Required parameter not found!", 1);
@@ -33,7 +33,262 @@ class CreatePassengerNameRecord{
 
     private function getRequest() {
         $request = '
-            { "CreatePassengerNameRecordRQ": { "targetCity": "3QND", "Profile": { "UniqueID": { "ID": "ABCD1EF" } }, "AirBook": { "OriginDestinationInformation": { "FlightSegment": [{ "ArrivalDateTime": "2017-04-30", "DepartureDateTime": "2019-07-30T13:55", "FlightNumber": "309", "NumberInParty": "1", "ResBookDesigCode": "V", "Status": "NN", "DestinationLocation": { "LocationCode": "KHI" }, "MarketingAirline": { "Code": "PK", "FlightNumber": "309" }, "MarriageGrp": "O", "OriginLocation": { "LocationCode": "ISB" } }] } }, "AirPrice": { "PriceRequestInformation": { "OptionalQualifiers": { "MiscQualifiers": { "TourCode": { "Text": "TEST1212" } }, "PricingQualifiers": { "PassengerType": [{ "Code": "CNN", "Quantity": "1" }] } } } }, "MiscSegment": { "DepartureDateTime": "2019-07-30", "NumberInParty": 1, "Status": "NN", "Type": "OTH", "OriginLocation": { "LocationCode": "ISB" }, "Text": "TEST", "VendorPrefs": { "Airline": { "Code": "PK" } } }, "SpecialReqDetails": { "AddRemark": { "RemarkInfo": { "FOP_Remark": { "Type": "CHECK", "CC_Info": { "Suppress": true, "PaymentCard": { "AirlineCode": "PK", "CardSecurityCode": "1234", "Code": "VI", "ExpireDate": "2012-12", "ExtendedPayment": "12", "ManualApprovalCode": "123456", "Number": "4123412341234123", "SuppressApprovalCode": true } } }, "FutureQueuePlaceRemark": { "Date": "12-21", "PrefatoryInstructionCode": "11", "PseudoCityCode": "IPCC1", "QueueIdentifier": "499", "Time": "06:00" }, "Remark": [{ "Type": "Historical", "Text": "TEST HISTORICAL REMARK" }, { "Type": "Invoice", "Text": "TEST INVOICE REMARK" }, { "Type": "Itinerary", "Text": "TEST ITINERARY REMARK" }, { "Type": "Hidden", "Text": "TEST HIDDEN REMARK" }] } }, "AirSeat": { "Seats": { "Seat": [{ "NameNumber": "1.1", "Preference": "AN", "SegmentNumber": "0" }, { "NameNumber": "2.1", "Preference": "AN", "SegmentNumber": "1" }, { "NameNumber": "3.1", "Preference": "AN", "SegmentNumber": "1" }] } }, "SpecialService": { "SpecialServiceInfo": { "Service": [{ "SSR_Code": "OSI", "PersonName": { "NameNumber": "testing" #}, "Text": "TEST1", "VendorPrefs": { "Airline": { "Code": "PK" } } }] } } }, "PostProcessing": { "RedisplayReservation": true, "ARUNK": "", "QueuePlace": { "QueueInfo": { "QueueIdentifier": [{ "Number": "100", "PrefatoryInstructionCode": "11" }] } }, "EndTransaction": { "Source": { "ReceivedFrom": "SWS TEST" } } } } }
+        {
+            "CreatePassengerNameRecordRQ": {
+              "version": "2.2.0",
+              "targetCity": "TM61",
+              "haltOnAirPriceError": false,
+              "TravelItineraryAddInfo": {
+                "AgencyInfo": {
+                  "Address": {
+                    "AddressLine": "SABRE TRAVEL",
+                    "CityName": "SOUTHLAKE",
+                    "CountryCode": "US",
+                    "PostalCode": "76092",
+                    "StateCountyProv": {
+                      "StateCode": "TX"
+                    },
+                    "StreetNmbr": "3150 SABRE DRIVE"
+                  },
+                  "Ticketing": {
+                    "TicketType": "7TAW"
+                  }
+                },
+                "CustomerInfo": {
+                  "ContactNumbers": {
+                    "ContactNumber": [
+                      {
+                        "NameNumber": "1.1",
+                        "Phone": "817-555-1212",
+                        "PhoneUseType": "H"
+                      }
+                    ]
+                  },
+                  "PersonName": [
+                    {
+                      "NameNumber": "1.1",
+                      "NameReference": "ABC123",
+                      "PassengerType": "ADT",
+                      "GivenName": "MARCIN",
+                      "Surname": "DZIK"
+                    }
+                  ]
+                }
+              },
+              "AirBook": {
+                "HaltOnStatus": [
+                  {
+                    "Code": "HL"
+                  },
+                  {
+                    "Code": "KK"
+                  },
+                  {
+                    "Code": "LL"
+                  },
+                  {
+                    "Code": "NN"
+                  },
+                  {
+                    "Code": "NO"
+                  },
+                  {
+                    "Code": "UC"
+                  },
+                  {
+                    "Code": "US"
+                  }
+                ],
+                "OriginDestinationInformation": {
+                  "FlightSegment": [
+                    {
+                      "ArrivalDateTime": "2019-09-15T23:01:00",
+                      "DepartureDateTime": "2019-09-15T18:15:00",
+                      "FlightNumber": "2609",
+                      "NumberInParty": "1",
+                      "ResBookDesigCode": "Y",
+                      "Status": "NN",
+                      "DestinationLocation": {
+                        "LocationCode": "DFW"
+                      },
+                      "MarketingAirline": {
+                        "Code": "AA",
+                        "FlightNumber": "2609"
+                      },
+                      "OriginLocation": {
+                        "LocationCode": "LAS"
+                      }
+                    }
+                  ]
+                },
+                "RedisplayReservation": {
+                  "NumAttempts": 10,
+                  "WaitInterval": 300
+                }
+              },
+              "AirPrice": [
+                {
+                  "PriceRequestInformation": {
+                    "Retain": true,
+                    "OptionalQualifiers": {
+                      "FOP_Qualifiers": {
+                        "BasicFOP": {
+                          "Type": "CK"
+                        }
+                      },
+                      "PricingQualifiers": {
+                        "PassengerType": [
+                          {
+                            "Code": "ADT",
+                            "Quantity": "1"
+                          }
+                        ]
+                      }
+                    }
+                  }
+                }
+              ],
+              "HotelBook": {
+                "BookingInfo": {
+                  "BookingKey": "80c8aac8-bc75-42f7-9266-339103f1258d",
+                  "RequestorID": "SG000000"
+                },
+                "Rooms": {
+                  "Room": [
+                    {
+                      "Guests": {
+                        "Guest": [
+                          {
+                            "Contact": {
+                              "Phone": "817-555-1212"
+                            },
+                            "FirstName": "MARCIN",
+                            "LastName": "DZIK",
+                            "Index": 1,
+                            "LeadGuest": true,
+                            "Type": 10,
+                            "Email": "Witold.Petriczek@sabre.com"
+                          }
+                        ]
+                      },
+                      "RoomIndex": 1
+                    }
+                  ]
+                },
+                "PaymentInformation": {
+                  "FormOfPayment": {
+                    "PaymentCard": {
+                      "PaymentType": "CC",
+                      "CardCode": "VI",
+                      "CardNumber": "4000000000006",
+                      "ExpiryMonth": 6,
+                      "ExpiryYear": "2021",
+                      "FullCardHolderName": {
+                        "FirstName": "MARCIN",
+                        "LastName": "DZIK",
+                        "Email": "Witold.Petriczek@sabre.com"
+                      },
+                      "CSC": "123",
+                      "Address": {
+                        "AddressLine": [
+                          "Wadowicka 6"
+                        ],
+                        "CityName": "Krakow",
+                        "StateProvince": {
+                          "code": "KR"
+                        },
+                        "StateProvinceCodes": {
+                          "Code": [
+                            {
+                              "content": "KR"
+                            }
+                          ]
+                        },
+                        "PostCode": "30-415",
+                        "CountryCodes": {
+                          "Code": [
+                            {
+                              "content": "PL"
+                            }
+                          ]
+                        }
+                      },
+                      "Phone": {
+                        "PhoneNumber": "817-555-1212"
+                      }
+                    }
+                  },
+                  "Type": "GUARANTEE"
+                },
+                "POS": {
+                  "Source": {
+                    "RequestorID": {
+                      "Type": 5,
+                      "Id": "12345678",
+                      "IdContext": "IATA"
+                    },
+                    "AgencyAddress": {
+                      "AddressLine1": "3150 SABRE DRIVE",
+                      "CityName": {},
+                      "CountryName": {
+                        "Code": "US"
+                      }
+                    },
+                    "AgencyName": "Really Trustworthy Agency",
+                    "ISOCountryCode": "US",
+                    "PseudoCityCode": "TM61"
+                  }
+                }
+              },
+              "SpecialReqDetails": {
+                "AddRemark": {
+                  "RemarkInfo": {
+                    "FOP_Remark": {
+                      "Type": "CHECK"
+                    }
+                  }
+                },
+                "SpecialService": {
+                  "SpecialServiceInfo": {
+                    "SecureFlight": [
+                      {
+                        "SegmentNumber": "A",
+                        "PersonName": {
+                          "DateOfBirth": "2001-01-01",
+                          "Gender": "M",
+                          "NameNumber": "1.1",
+                          "GivenName": "MARCIN",
+                          "Surname": "DZIK"
+                        },
+                        "VendorPrefs": {
+                          "Airline": {
+                            "Hosted": true
+                          }
+                        }
+                      }
+                    ],
+                    "Service": [
+                      {
+                        "SSR_Code": "OTHS",
+                        "Text": "CC MARCIN DZIK"
+                      }
+                    ]
+                  }
+                }
+              },
+              "PostProcessing": {
+                "ARUNK": {},
+                "EndTransaction": {
+                  "Source": {
+                    "ReceivedFrom": "SP TEST"
+                  }
+                },
+                "RedisplayReservation": {
+                  "waitInterval": 100
+                }
+              }
+            }
+          }
         ';
         return $request;
     }
--- src/Rest/Auth.php
+++ src/Rest/Auth.php
@@ -5,6 +5,8 @@ class Auth{
     public function __construct() {
         $this->config = config('sabre')[config('sabre.env')];
         $this->url = $this->config['rest'].'/v2/auth/token';
+        // $this->url = 'https://api.havail.sabre.com/v2/auth/token';
+        // dd($this->url);
     }
 
     public function callForToken() {
@@ -13,7 +15,8 @@ class Auth{
         $headers = array(
             'Authorization: Basic '.$this->buildCredentials(),
             'Accept: */*',
-            'Content-Type: application/x-www-form-urlencoded'
+            'Content-Type: application/x-www-form-urlencoded',
+            'grant_type=client_credentials'
         );
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_POST, 1);
@@ -23,6 +26,7 @@ class Auth{
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
         curl_close($ch);
+        // dd($result);
         return json_decode($result);
     }
     
@@ -31,8 +35,14 @@ class Auth{
                 $this->config["userId"].":".
                 $this->config["group"].":".
                 $this->config["domain"];
-        $secret = base64_encode($this->config["clientSecret"]);
-        return base64_encode(base64_encode($credentials).":".$secret);
+
+        $b64Credentials = base64_encode($credentials);        
+        $secret = $this->config["clientSecret"];
+        $b64Secret = base64_encode($secret);
+        $token = $b64Credentials.":".$b64Secret;
+        $b64Token = base64_encode($token);
+        // dd($credentials, $b64Credentials, $secret, $b64Secret, $token, $b64Token);
+        return $b64Token;
     }
 
 }
